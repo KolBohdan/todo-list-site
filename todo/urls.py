1f900682d97bd7e93f2f72a5d502fd9316905e9a
Bohdan Kolomiiets <kb907406@gmail.com>
2023-11-08 14:52:34 +0200

    Add todo app to setting.py, create simple page for Tag model

--- todo/urls.py
+++ todo/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from .views import (
     index,
+    TagListView,
 )
 
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
 ]
 
 app_name = "todo"
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views import generic
+
+from todo.models import Tag
+
 
 def index(request: HttpRequest) -> HttpResponse:
     return render(request, "todo/index.html")
+
+
+class TagListView(generic.ListView):
+    model = Tag
