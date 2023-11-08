f8b1e1679bb673513ca5a011c7fa0ff8b820940e
Bohdan Kolomiiets <kb907406@gmail.com>
2023-11-08 14:32:38 +0200

    Add Tag and Task models with ManyToMany relation

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+
+class Task(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField()
+    is_done = models.BooleanField(default=False)
+    tags = models.ManyToManyField(
+        to=Tag,
+        related_name="tasks"
+    )
