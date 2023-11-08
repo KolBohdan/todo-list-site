f1a399b85f1ac327e7ab8186e3095d9fc18aefe4
Bohdan Kolomiiets <kb907406@gmail.com>
2023-11-08 15:53:43 +0200

    Implement CRUD for the Tag model, add crispy bootstrap

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
 from todo.models import Tag
@@ -11,3 +12,20 @@ def index(request: HttpRequest) -> HttpResponse:
 
 class TagListView(generic.ListView):
     model = Tag
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("todo:tag-list")
